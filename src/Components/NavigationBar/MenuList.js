7df97d14694bd9e99ad47ac1dad89db2dfe3891a
G4tLan <langagift01@gmail.com>
2019-10-16 23:44:15 +0200

    toggling menu list

--- src/Components/NavigationBar/MenuList.js
+++ src/Components/NavigationBar/MenuList.js
@@ -1,10 +1,30 @@
 import React from 'react';
 
 class MenuList extends React.Component {
+    constructor(){
+        super();
+
+        this.state = {
+            showMenu: true,
+        };
+    }
+    
+    hideMenu = ()=>{
+        this.setState({
+            showMenu: false,
+        })
+    }
     render(){
         return (
-            <div className="menu-list-container">
-                Menu List
+            <div className={`menu-list-container${this.state.showMenu?'':
+                    '-minimize'
+                }`}>
+                <div className="menu-list">
+                    Menu List
+                </div>
+                <div className="toggle" onClick={this.hideMenu}>
+                    
+                </div>
             </div>
         )
     }
