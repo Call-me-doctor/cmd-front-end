db2b10b24b396ed0b4eef3d9fb5f1aa5d4848f17
G4tLan <langagift01@gmail.com>
2019-10-17 22:03:58 +0200

    showing menu items

--- src/Components/NavigationBar/MenuList.js
+++ src/Components/NavigationBar/MenuList.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import {MENU_LIST} from '../../constants';
 
 class MenuList extends React.Component {
     
@@ -14,8 +15,10 @@ class MenuList extends React.Component {
             <div className={`menu-list-container${this.props.showMenu?'':' minimize'
                 }`}>
                 <div className="menu-list">
-                    {this.menuItem('About', 'about')}
-                    {this.menuItem('FAQs', 'faqs')}
+                    {this.menuItem(MENU_LIST.register, 'register')}
+                    {this.menuItem(MENU_LIST.about, 'about')}
+                    {this.menuItem(MENU_LIST.faqs, 'faqs')}
+                    {this.menuItem(MENU_LIST.terms_and_conditions, 'terms_and_conditions')}
                 </div>
                 <div className="toggle" onClick={this.props.toggle}>
                     
--- src/constants.js
+++ src/constants.js
@@ -1,11 +1,7 @@
-MENU_LIST = {
+export const MENU_LIST = {
     about: 'About',
     login: 'Login',
     register: 'Register',
     faqs: 'FAQs',
     terms_and_conditions: 'Terms & Conditions',
-}
-
-export default {
-    MENU_LIST,
 }
