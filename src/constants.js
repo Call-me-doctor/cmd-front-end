0d0b096349886431c883e66aaad64e5e8615c765
G4tLan <langagift01@gmail.com>
2019-10-17 21:53:16 +0200

    js constants

---
+++ src/constants.js
@@ -0,0 +1,11 @@
+MENU_LIST = {
+    about: 'About',
+    login: 'Login',
+    register: 'Register',
+    faqs: 'FAQs',
+    terms_and_conditions: 'Terms & Conditions',
+}
+
+export default {
+    MENU_LIST,
+}
