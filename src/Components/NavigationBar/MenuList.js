37f4b3ec3348b88a099d23aa73028cf006c3da8f
G4tLan <langagift01@gmail.com>
2019-10-17 21:51:45 +0200

    menu item

--- src/Components/NavigationBar/MenuList.js
+++ src/Components/NavigationBar/MenuList.js
@@ -2,12 +2,20 @@ import React from 'react';
 
 class MenuList extends React.Component {
     
+    menuItem = (title, url) => {
+        return (
+            <div>
+                <a href={url}>{title}</a>
+            </div>
+        )
+    }
     render(){
         return (
             <div className={`menu-list-container${this.props.showMenu?'':' minimize'
                 }`}>
                 <div className="menu-list">
-                    Menu List
+                    {this.menuItem('About', 'about')}
+                    {this.menuItem('FAQs', 'faqs')}
                 </div>
                 <div className="toggle" onClick={this.props.toggle}>
                     
