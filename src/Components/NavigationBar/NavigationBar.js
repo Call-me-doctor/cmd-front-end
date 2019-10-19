a4aae23cba4321fc45b948efdf9e802642c5d674
G4tLan <langagift01@gmail.com>
2019-10-19 14:13:10 +0200

    desktop menu

--- src/Components/NavigationBar/NavigationBar.js
+++ src/Components/NavigationBar/NavigationBar.js
@@ -2,6 +2,7 @@ import React from 'react';
 import MenuIcon from '@material-ui/icons/Menu';
 
 import MenuList from './MenuList';
+import {MENU_LIST} from '../../constants';
 
 class NavigationBar extends React.Component {
     constructor(){
@@ -17,6 +18,14 @@ class NavigationBar extends React.Component {
         })
     }
 
+    menuItem = (title, url) => {
+        return (
+            <div className="link">
+                <a href={url}>{title}</a>
+            </div>
+        )
+    }
+
     render(){
         return (
             <div className="navbar">
@@ -27,6 +36,11 @@ class NavigationBar extends React.Component {
                     </div>
                     <div className="logo">
                         <img src="images/logo_trans.png" alt="logo"/>
+                    </div>
+                    <div className="menu-items">
+                        {this.menuItem(MENU_LIST.about, 'about')}
+                        {this.menuItem(MENU_LIST.faqs, 'faqs')}
+                        {this.menuItem(MENU_LIST.terms_and_conditions, 'terms_and_conditions')}
                     </div>
                 </div>
                 <div className="login">
