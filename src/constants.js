656c92063d3053725815a0c08219569a38b2f4f2
G4tLan <langagift01@gmail.com>
2019-10-19 14:48:50 +0200

    add lorem

--- src/constants.js
+++ src/constants.js
@@ -5,3 +5,15 @@ export const MENU_LIST = {
     faqs: 'FAQs',
     terms_and_conditions: 'Terms & Conditions',
 }
+
+export const LOREM = 'Lorem ipsum dolor sit amet, \
+             consectetur adipiscing elit, sed do eiusmod \
+             tempor incididunt ut labore et dolore magna \
+             aliqua. Ut enim ad minim veniam, quis nostrud \
+             exercitation ullamco laboris nisi ut aliquip \
+             ex ea commodo consequat. \ \n\n \
+             Duis aute irure dolor in reprehenderit in \
+             voluptate velit esse cillum dolore eu fugiat \
+             nulla pariatur. Excepteur sint occaecat \
+             cupidatat non proident, sunt in culpa qui \
+             officia deserunt mollit anim id est laborum.'
