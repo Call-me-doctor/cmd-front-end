96a697769eb4a377b1aff2102007f2fda7a860a2
G4tLan <langagift01@gmail.com>
2019-10-19 15:05:51 +0200

    fix warnig

--- src/constants.js
+++ src/constants.js
@@ -6,14 +6,15 @@ export const MENU_LIST = {
     terms_and_conditions: 'Terms & Conditions',
 }
 
-export const LOREM = 'Lorem ipsum dolor sit amet, \
-             consectetur adipiscing elit, sed do eiusmod \
-             tempor incididunt ut labore et dolore magna \
-             aliqua. Ut enim ad minim veniam, quis nostrud \
-             exercitation ullamco laboris nisi ut aliquip \
-             ex ea commodo consequat. <br /> <br /> \
-             Duis aute irure dolor in reprehenderit in \
-             voluptate velit esse cillum dolore eu fugiat \
-             nulla pariatur. Excepteur sint occaecat \
-             cupidatat non proident, sunt in culpa qui \
-             officia deserunt mollit anim id est laborum.'
+export const LOREM = `Lorem ipsum dolor sit amet, 
+             consectetur adipiscing elit, sed do eiusmod 
+             tempor incididunt ut labore et dolore magna 
+             aliqua. Ut enim ad minim veniam, quis nostrud 
+             exercitation ullamco laboris nisi ut aliquip 
+             ex ea commodo consequat.
+             \n
+             Duis aute irure dolor in reprehenderit in 
+             voluptate velit esse cillum dolore eu fugiat 
+             nulla pariatur. Excepteur sint occaecat 
+             cupidatat non proident, sunt in culpa qui 
+             officia deserunt mollit anim id est laborum.`
