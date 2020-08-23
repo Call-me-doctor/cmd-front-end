26d0480075de98e9325e33397e89fe92bdd70ff3
GiftLanga <glanga@retrorabbit.co.za>
2020-08-23 20:07:38 +0200

    View appointment init

---
+++ src/Components/ViewAppointment.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react';
+
+class ViewAppointment extends Component {
+  render() {
+    return (
+      <div>
+        View Appointment
+      </div>
+    );
+  }
+}
+
+export default ViewAppointment;
