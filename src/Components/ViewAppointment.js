75d30e35538e0ddf0d25233eaa2b1932500bbf38
GiftLanga <glanga@retrorabbit.co.za>
2020-08-30 15:55:39 +0200

    fix error

--- src/Components/ViewAppointment.js
+++ src/Components/ViewAppointment.js
@@ -1,5 +1,7 @@
 import React, { Component, Fragment } from 'react';
 import BackNavBar from './NavigationBar/BackNavBar';
+import Avatar from './Avatar';
+
 class ViewAppointment extends Component {
   render() {
     return (
