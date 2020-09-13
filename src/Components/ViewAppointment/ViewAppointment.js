98871275083f71b2998a69d7f3fef82c2ffe1a2e
GiftLanga <glanga@retrorabbit.co.za>
2020-09-13 14:04:25 +0200

    fix styling

--- src/Components/ViewAppointment/ViewAppointment.js
+++ src/Components/ViewAppointment/ViewAppointment.js
@@ -1,6 +1,8 @@
 import React, { Component, Fragment } from 'react';
 import BackNavBar from '../NavigationBar/BackNavBar';
 import Avatar from '../Avatar';
+import Divider from '../Divider';
+import { LOREM } from '../../constants';
 import './ViewAppointment.scss';
 
 class ViewAppointment extends Component {
@@ -15,6 +17,21 @@ class ViewAppointment extends Component {
             <label className="sub-title">Hospital name</label>
             <label className="sub-title">Date: 12/05/2020</label>
             <label className="sub-title">Status</label>
+          </div>
+        </div>
+        <Divider />
+        <div className="appointment-details">
+          <div className="detail">
+            <label className="body-title">Affected Body Parts</label>
+            <div className="description">Head, nose</div>
+          </div>
+          <div className="detail">
+            <label className="body-title">Description</label>
+            <div className="description">{LOREM}</div>
+          </div>
+          <div className="detail">
+            <label className="body-title">Hospital Location</label>
+            <div className="description">{LOREM}</div>
           </div>
         </div>
       </Fragment>
